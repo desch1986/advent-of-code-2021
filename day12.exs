795251ee6446f8ad302f89ebff1c4b2f6859a9e0
Dennis Schiavo <github@dschiavo.net>
2021-12-18 13:19:45 +0100

    Day 12 solution rewritten and cleaned up with best practices

--- day12.exs
+++ day12.exs
@@ -1,62 +1,55 @@
 defmodule Day12 do
   def part1(path) do
     path
-    |> File.read!
-    |> String.split("\n", trim: true)
-    |> parse
-    |> find_path1("start", [])
-    |> List.flatten()
-    |> Enum.count(&(&1 == "end"))
+    |> parse()
+    |> find_paths(false)
   end
 
   def part2(path) do
     path
-    |> File.read!
-    |> String.split("\r\n", trim: true)
-    |> find_path2("start", [], true)
-    |> List.flatten()
-    |> Enum.count(&(&1 == "end"))
+    |> parse()
+    |> find_paths(true)
   end
 
-  defp parse(lines) do
-    caves = lines 
-    |> Enum.map(&String.split(&1, "-", trim: true))
+  defp find_paths(connections, visit_twice, position \\ "start", visited \\ MapSet.new())
+  defp find_paths(_, _, "end", _), do: 1
+  defp find_paths(connections, visit_twice, position, visited) do
+    neighbors = Map.get(connections, position, [])
 
-    caves
-    |> Enum.map(&Enum.reverse/1)
-    |> Enum.concat(caves)
-    |> Enum.group_by(&Enum.at(&1, 0), &Enum.at(&1, 1))
-  end
-
-  defp find_path1(_, "end", visited), do: ["end" | visited]
-  defp find_path1(map, current_cave, visited) do
-    map
-    |> Map.get(current_cave)
-    |> Enum.filter(fn cave -> 
-      cave == String.upcase(cave) || cave not in visited 
-    end)
-    |> Enum.map(fn cave ->
-      find_path1(map, cave, [current_cave | visited])
-    end)
-  end
-
-  defp find_path2(_, "end", visited, _), do: ["end" | visited]
-  defp find_path2(_, "start", [_|_] = visited, _), do: visited
-  defp find_path2(map, current_cave, visited, small) do
-    small =
+    neighbors
+    |> Enum.reject(&(&1 == "start"))
+    |> Enum.map(fn neighbor ->
       cond do
-        small == false -> false
-        current_cave == String.downcase(current_cave) && current_cave in visited -> false
-        true -> true
-      end
+        is_small_cave(neighbor) and neighbor in visited and visit_twice ->
+          find_paths(connections, false, neighbor, MapSet.put(visited, position))
 
-    map
-    |> Map.get(current_cave)
-    |> Enum.filter(fn cave -> 
-      small || cave == String.upcase(cave) || cave not in visited 
+        is_small_cave(neighbor) and neighbor in visited ->
+          0
+
+        true ->
+          find_paths(
+            connections,
+            visit_twice,
+            neighbor,
+            MapSet.put(visited, position)
+          )
+      end
     end)
-    |> Enum.map(fn cave ->
-      find_path2(map, cave, [current_cave | visited], small)
-    end)
-  end  
+    |> Enum.sum()
+  end
+
+  defp is_small_cave(cave), do: cave == String.downcase(cave)
+
+  defp parse(path) do
+    input = path
+    |> File.read!
+    for row <- String.split(input, "\r\n", trim: true),
+        [from, to] = String.split(row, "-", parts: 2),
+        reduce: %{} do
+      connections ->
+        connections
+        |> Map.update(from, [to], fn dests -> [to | dests] end)
+        |> Map.update(to, [from], fn dests -> [from | dests] end)
+    end
+  end
 end
